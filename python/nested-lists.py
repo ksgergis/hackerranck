6292bcf0b4c2dc50ed5a17b300fe7dfda81e3563
M975851 <karim.gergis@macys.com>
2017-03-21 14:30:27 -0700

    fixing indentation

--- python/nested-lists.py
+++ python/nested-lists.py
@@ -1,4 +1,4 @@
-#https://www.hackerrank.com/challenges/nested-list
+# question link: https://www.hackerrank.com/challenges/nested-list
 
 #This question solved part of learning Python series
 #Goal to learn creation of list of lists
@@ -11,20 +11,20 @@
 import heapq
 if __name__ == '__main__':
 	#loading the data from stdin
-    gradeList = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        heapq.heappush(gradeList,(score,name))
+	gradeList = []
+	for _ in range(int(input())):
+		name = input()
+		score = float(input())
+		heapq.heappush(gradeList,(score,name))
     
-    #get the smallest grade, skip all the students who got same grade
-    smallest = heapq.heappop(gradeList)[0]
-    while(heapq.nsmallest(1,gradeList)[0][0] == smallest ):
-        heapq.heappop(gradeList)
+	#get the smallest grade, skip all the students who got same grade
+	smallest = heapq.heappop(gradeList)[0]
+	while(heapq.nsmallest(1,gradeList)[0][0] == smallest ):
+		heapq.heappop(gradeList)
     
-    #get the second smallest grade, print the name of all students who got this grade ordered Alphabetically
-    secondSmallest = heapq.heappop(gradeList)
-    print(secondSmallest[1])
-    while(heapq.nsmallest(1,gradeList)[0][0] == secondSmallest[0]):
-        print(heapq.heappop(gradeList)[1])
+	#get the second smallest grade, print the name of all students who got this grade ordered Alphabetically
+	secondSmallest = heapq.heappop(gradeList)
+	print(secondSmallest[1])
+	while(heapq.nsmallest(1,gradeList)[0][0] == secondSmallest[0]):
+		print(heapq.heappop(gradeList)[1])
  
