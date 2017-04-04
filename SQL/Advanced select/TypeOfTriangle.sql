e96f5e4f710581ed9abe0dd83b6dd5c0ce983c98
ksger <ksger@DESKTOP-CLIO4RQ>
2017-04-04 10:44:34 -0700

    .

---
+++ SQL/Advanced select/TypeOfTriangle.sql
@@ -0,0 +1,8 @@
+select  
+    (CASE 
+        WHEN a=b and a=c THEN 'Equilateral'
+        WHEN (a=b or a=c or b=c) and(a+b > c and a+c> b and b+c> a) THEN 'Isosceles' 
+        WHEN a+b > c and a+c> b and b+c> a THEN 'Scalene'
+        ELSE "Not A Triangle"
+    END) AS tType
+ from TRIANGLES 
